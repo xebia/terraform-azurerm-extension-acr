701421219a056c4a16aab03ef1bddbd066f05af9
Patrick de Kruijf <93589964+patrick-de-kruijf@users.noreply.github.com>
2025-12-01 12:19:04 +0100

    Delete azuread_group and AcrPull role assignment
    Removed azuread_group and related role assignment.

--- src/main.tf
+++ src/main.tf
@@ -1,15 +1,3 @@
-resource "azuread_group" "spoke_acrpull_group" {
-    display_name = var.acr_group_name
-    owners = [var.service_principal_object_id]
-    administrative_unit_ids = var.acr_group_administrative_units
-    security_enabled = true
-}
-
-resource "azurerm_role_assignment" "acr_group_pull" {
-  scope                = var.acr_resource_id 
-  role_definition_name = "AcrPull"
-  principal_id         = azuread_group.spoke_acrpull_group.object_id
-}
 
 resource "azurerm_role_assignment" "spoke_contributors_acrpull" {
   scope                = var.acr_resource_id 

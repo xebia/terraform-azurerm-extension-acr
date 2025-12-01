41123c096edf60bfafe032049eb52e5b240d0701
patryk.gasiorowski <patryk.gasiorowski@xebia.com>
2025-12-01 13:09:00 +0100

    Add Managed Identities and role assignments for ACR access

--- src/main.tf
+++ src/main.tf
@@ -28,3 +28,57 @@ resource "azurerm_role_assignment" "spoke_readers_reader" {
   role_definition_name = "Reader"
   principal_id         = var.reader_group_id
 }
+
+# Managed Identities for Pull, Push, Reader
+resource "azurerm_user_assigned_identity" "spoke_pull_mi" {
+  name                = "${var.spoke_name}-acrpull-mi"
+  resource_group_name = var.spoke_resource_group_name
+  location            = var.spoke_location
+}
+
+resource "azurerm_user_assigned_identity" "spoke_push_mi" {
+  name                = "${var.spoke_name}-acrpush-mi"
+  resource_group_name = var.spoke_resource_group_name
+  location            = var.spoke_location
+}
+
+resource "azurerm_user_assigned_identity" "spoke_reader_mi" {
+  name                = "${var.spoke_name}-acrreader-mi"
+  resource_group_name = var.spoke_resource_group_name
+  location            = var.spoke_location
+}
+
+# Add Managed Identities to correct AD group
+resource "azuread_group_member" "pull_mi_member" {
+  group_object_id = azuread_group.spoke_acrpull_group.object_id
+  member_object_id = azurerm_user_assigned_identity.spoke_pull_mi.principal_id
+}
+
+resource "azuread_group_member" "push_mi_member" {
+  group_object_id = var.owner_group_id
+  member_object_id = azurerm_user_assigned_identity.spoke_push_mi.principal_id
+}
+
+resource "azuread_group_member" "reader_mi_member" {
+  group_object_id = var.reader_group_id
+  member_object_id = azurerm_user_assigned_identity.spoke_reader_mi.principal_id
+}
+
+# Assign ACR roles to Managed Identities
+resource "azurerm_role_assignment" "pull_mi_acrpull" {
+  scope                = var.acr_resource_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.spoke_pull_mi.principal_id
+}
+
+resource "azurerm_role_assignment" "push_mi_acrpush" {
+  scope                = var.acr_resource_id
+  role_definition_name = "AcrPush"
+  principal_id         = azurerm_user_assigned_identity.spoke_push_mi.principal_id
+}
+
+resource "azurerm_role_assignment" "reader_mi_reader" {
+  scope                = var.acr_resource_id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_user_assigned_identity.spoke_reader_mi.principal_id
+}
--- src/variables_global.tf
+++ src/variables_global.tf
@@ -1,3 +1,7 @@
+variable "spoke_name" {
+  description = "The name prefix for the spoke resources and managed identities."
+  type        = string
+}
 variable "service_principal_object_id" {
   description = "The ID of the service principal of the spoke."
   type        = string
@@ -15,5 +19,15 @@ variable "contributor_group_id" {
 
 variable "reader_group_id" {
   description = "The ID of the PIM Reader group of the spoke."
+  type        = string
+}
+
+variable "spoke_resource_group_name" {
+  description = "The name of the spoke resource group where managed identities will be created."
+  type        = string
+}
+
+variable "spoke_location" {
+  description = "The Azure location of the spoke resource group."
   type        = string
 }

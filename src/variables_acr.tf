f653d4cb8a8eb3e4951c8c75479b76c4697bdb12
Patrick de Kruijf <93589964+patrick-de-kruijf@users.noreply.github.com>
2025-12-01 12:48:21 +0100

    Add new variables for ACR group and administrative units

--- src/variables_acr.tf
+++ src/variables_acr.tf
@@ -2,3 +2,14 @@ variable "acr_resource_id" {
     description = "The resource ID of the container registry to grant access to"
     type = string
 }
+
+variable "acr_group_name" {
+  description = "The name of the group to create"
+  type        = string
+}
+
+variable "acr_group_administrative_units" {
+  description = "The list of administrative unit IDs the spoke acrpull group should belong to"
+  type        = list(string)
+  default     = []
+}
